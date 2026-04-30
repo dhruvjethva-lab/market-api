b7cc333cf0e130b423e415aac7002cbf9e0f9893
dhruvjethva-lab <dhruvjethva@aamanfinserv.com>
2026-04-30 13:57:19 +0530

    Update market.js

--- api/market.js
+++ api/market.js
@@ -1,24 +1,15 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch(
-  "https://yahoo-finance15.p.rapidapi.com/api/v1/markets/quote?symbols=%5ENSEI,%5ENSEBANK,%5EBSESN",
-      {
-        method: "GET",
-        headers: {
-          headers: {
-  "X-RapidAPI-Key": "def87488efmsh4220ccf1af1f7d3p16c92bjsn9d9c353fdbb7",
-  "X-RapidAPI-Host": "yahoo-finance15.p.rapidapi.com"
-        }
-      }
-    );
+    const url = "https://query1.finance.yahoo.com/v7/finance/quote?symbols=%5ENSEI,%5ENSEBANK,%5EBSESN";
 
+    const response = await fetch(url);
     const data = await response.json();
 
     res.status(200).json(data);
 
   } catch (error) {
     res.status(500).json({
-      error: "API failed",
+      error: "Failed to fetch data",
       details: error.message
     });
   }
